436deea5ab99aaed7b664ad409aefc6f5abe8f77
harmonics12 <145725336+harmonics12@users.noreply.github.com>
2025-06-06 08:35:27 +1000

    Remove static export to troubleshoot Cloudflare build

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  }
+  // Remove static export for now to troubleshoot
 };
 
 export default nextConfig;
